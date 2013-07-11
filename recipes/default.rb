5974b5fc3f1c8089ba8a4e41bb8ce83c1f9a2ac2
mbman <mario.baricevic@gmail.com>
2013-07-11 10:48:34 +0200

    Mysql allow remote connections and bind address 192.168.56.101

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,15 @@ node.set['apache']['default_modules'] = [
     "php5"
 ]
 node.set['apache']['default_site_enabled'] = true
+
 node.set['mysql']['server_root_password'] = "foobar"
 node.set['mysql']['server_repl_password'] = "foobar"
 node.set['mysql']['server_debian_password'] = "foobar"
-node.set['mysql']['allow_remote_root'] = true
+node.set['mysql']['remove_anonymous_users'] = true
 node.set['mysql']['remove_test_database'] = true
+
+# allow mysqladmin connections from any host
+node.set['mysql']['allow_remote_root'] = true
 
 include_recipe "apt"
 include_recipe "openssl"
